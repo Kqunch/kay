aaca98ea83ada43d448113c40fb7e2e2f7ca00f6
Kqunch <38544203+Kqunch@users.noreply.github.com>
2018-05-07 16:57:34 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,20 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-    
+    if (message.content === 'ping') {
+    	sendmessage('pong');
   	}
 });
 
