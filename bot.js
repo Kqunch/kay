d10abefae74347d487946780eaf48540320a98cc
Kqunch <38544203+Kqunch@users.noreply.github.com>
2018-05-07 17:11:37 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,21 +5,17 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if(message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
+client.on('message', function (user, userID, channelID, message, evt) {
+    if(message.content (0, 1) == '!') {
+    	 args = args.splice(1);
         switch(cmd) {
             // !ping
-            case 'string':
+            case 'ping':
                 bot.sendMessage({
                     to: channelID,
-                    message: 'yes'
+                    message: 'Pong!'
                 });
-  	}
-});
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
