30dc76be43e7f2b3b973b288a10e4a3648f8e735
Kqunch <38544203+Kqunch@users.noreply.github.com>
2018-05-07 17:57:32 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-   if (message.content === 'string') {
+    if (message.content === 'string') {
     	message.reply('yes');
   	}
 });
